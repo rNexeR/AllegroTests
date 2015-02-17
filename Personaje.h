39ec00dd6ec1255e2b7295670c6e52ed861b0c62
rnexer <rnexer@gmail.com>
2015-02-17 11:44:35 -0600

    Ahora en Clases

---
+++ Personaje.h
@@ -0,0 +1,35 @@
+#ifndef PERSONAJE_H
+#define PERSONAJE_H
+
+#include <iostream>
+using namespace std;
+
+#include <allegro5/allegro.h>
+#include "allegro5/allegro_image.h"
+
+enum MYKEYS {
+        KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
+};
+const float FPS = 60;
+const float var = 7.0;
+
+class Personaje
+{
+    public:
+        ALLEGRO_BITMAP  *image   = NULL;
+        ALLEGRO_TIMER *timer = NULL;
+        float image_x = 0.0, image_y = 0.0;
+
+        bool key[4] = { false, false, false, false };
+
+
+        Personaje(ALLEGRO_EVENT_QUEUE *event_queue);
+        void act(ALLEGRO_EVENT ev);
+        void draw();
+        void destroy();
+        virtual ~Personaje();
+    protected:
+    private:
+};
+
+#endif // PERSONAJE_H
--- main.cpp
+++ main.cpp
@@ -3,45 +3,19 @@
 using namespace std;
 #include <allegro5/allegro.h>
 #include "allegro5/allegro_image.h"
+#include "Personaje.h"
 
-enum MYKEYS {
-        KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
-};
-const float FPS = 60;
-const float var = 7.0;
+
 
 int main(int argc, char **argv){
 
    ALLEGRO_DISPLAY *display = NULL;
    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-   ALLEGRO_TIMER *timer = NULL;
-
-   ALLEGRO_BITMAP  *image   = NULL;
-   float image_x = 0.0, image_y = 0.0;
-
-    bool key[4] = { false, false, false, false };
 
 
    if(!al_init()) {
       fprintf(stderr, "failed to initialize allegro!\n");
       return -1;
-   }
-
-    if(!al_install_keyboard()) {
-      fprintf(stderr, "failed to initialize the keyboard!\n");
-      return -1;
-    }
-
-    timer = al_create_timer(1.0 / FPS);
-       if(!timer) {
-          fprintf(stderr, "failed to create timer!\n");
-          return -1;
-       }
-
-
-     if(!al_init_image_addon()) {
-      fprintf(stderr, "failed to initialize image addon!\n");
-      return 0;
    }
 
    display = al_create_display(640, 480);
@@ -57,23 +31,13 @@ int main(int argc, char **argv){
       return -1;
    }
 
-   image = al_load_bitmap("image.png");
-
-   if(!image) {
-      fprintf(stderr, "failed to load image!\n");
-      al_destroy_display(display);
-      return -1;
-   }
-
    al_register_event_source(event_queue, al_get_display_event_source(display));
-
-    al_register_event_source(event_queue, al_get_keyboard_event_source());
-
-    al_register_event_source(event_queue, al_get_timer_event_source(timer));
 
    al_clear_to_color(al_map_rgb(0,0,0));
 
    al_flip_display();
+
+    Personaje* per = new Personaje(event_queue);
 
    while(1)
    {
@@ -85,73 +49,20 @@ int main(int argc, char **argv){
 
       bool get_event = al_wait_for_event_until(event_queue, &ev, &timeout);
 
-            if(key[KEY_UP]) {
-                image_y -= var;
-            }
-
-            if(key[KEY_DOWN]) {
-                image_y += var;
-            }
-
-            if(key[KEY_LEFT]) {
-                image_x -= var;
-            }
-
-            if(key[KEY_RIGHT]) {
-                image_x += var;
-            }
-
         if(get_event && ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
             break;
-        }else if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
-         switch(ev.keyboard.keycode) {
-            case ALLEGRO_KEY_UP:
-               key[KEY_UP] = true;
-               break;
+        }
+    per->act(ev);
 
-            case ALLEGRO_KEY_DOWN:
-               key[KEY_DOWN] = true;
-               break;
-
-            case ALLEGRO_KEY_LEFT:
-               key[KEY_LEFT] = true;
-               break;
-
-            case ALLEGRO_KEY_RIGHT:
-               key[KEY_RIGHT] = true;
-               break;
-         }
-      }
-      else if(ev.type == ALLEGRO_EVENT_KEY_UP) {
-         switch(ev.keyboard.keycode) {
-            case ALLEGRO_KEY_UP:
-               key[KEY_UP] = false;
-               break;
-
-            case ALLEGRO_KEY_DOWN:
-               key[KEY_DOWN] = false;
-               break;
-
-            case ALLEGRO_KEY_LEFT:
-               key[KEY_LEFT] = false;
-               break;
-
-            case ALLEGRO_KEY_RIGHT:
-               key[KEY_RIGHT] = false;
-               break;
-         }
-    }
-
-
-      al_clear_to_color(al_map_rgb(0,0,0));
+      al_clear_to_color(al_map_rgb(150,0,255));
       //cout<<"Dibujando Imagen"<<endl;
-      al_draw_scaled_bitmap(image,0,0,200,400,image_x,image_y,100,100,0);
+    per->draw();
       al_flip_display();
    }
 
    al_destroy_display(display);
    al_destroy_event_queue(event_queue);
-   al_destroy_bitmap(image);
+
 
 
    return 0;
